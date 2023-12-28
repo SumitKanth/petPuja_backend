80726994390a7a184fb6cc38a83c2a0b3bc5b32f
sumit <sumitkanth999@gmail.com>
2023-12-29 00:38:39 +0530

    added cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ config({
 app.use(express.json());  // For getting req.body data
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://petpuja-backend.onrender.com/api/v1/user'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
